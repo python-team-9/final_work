f7aa927d104a4a7fb4b713da495457a4053742eb
xiaolongtuan <2765504026@qq.com>
2022-05-22 09:35:48 +0800

    窗口切换

---
+++ mainWindow/mianWindow.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('../Logup/')
+sys.path.append('../user/')
+
+import LogupWindow as logup
+import UserWindow as user
+
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class mainWindows:
+    def __init__(self):
+        self.logupW = logup.LoadingWindow()
+        self.logupW.switch.connect(self.switchWindows)
+        self.logupW.show()
+
+    def switchWindows(self):
+        self.client = self.logupW.client
+        self.id = self.logupW.userid
+        self.identity = self.logupW.identity
+        self.logupW.close()
+        if self.identity == 'users':
+            self.userW = user.UserWindow()
+            self.userW.show()
+        elif self.identity == 'bosses':
+            print("登录boss")
+        else:
+            print("登录managers")
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    main = mainWindows()
+    sys.exit(app.exec_())
